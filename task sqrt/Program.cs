e112d82060517663034f0b1d3dff7c0ac0422e01
Kostya <kosg@mail.ru>
2023-04-06 23:54:14 +0300

    add

---
+++ task sqrt/Program.cs
@@ -0,0 +1,12 @@
+﻿// квадратный корень
+
+int Sqrt(int x)
+{
+  for (int i = 2; i <= x / 2; i++)
+    if (i == x / i) return i;
+    return 1;
+}
+Console.Clear();
+Console.Write("enter number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Sqrt = {Sqrt(number)}");
